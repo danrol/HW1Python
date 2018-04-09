840155c8382bb91bce8b777acd28c190ca17f7e0
Eden Dupont <eg7eg7@gmail.com>
2018-04-09 20:40:23 +0300

    update to add_activity

--- project.py
+++ project.py
@@ -17,12 +17,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab
 
+
 class Graph():
     """This class represents project
         Attributes:
         activities_dict (activity_name : [{next_activity : activity_duration},
         {another_next_activity : anther_next_activity_duration}, ...]
     """
+
     def __init__(self, activities_dict=None, essential_activities=None):
         """ initializes a graph object
             If no dictionary or None is given, an empty dictionary will be used
@@ -38,12 +40,9 @@ class Graph():
         self._es_ls_ef_lf = dict()
         # TODO define default 0 for every value (4 values at all) in self._es_ls_ef_lf lists
         for activity_node, next_activities in self.activities_dict.items():
-            self._es_ls_ef_lf[activity_node] = [0]*4
+            self._es_ls_ef_lf[activity_node] = [0] * 4
 
         self._project_duration = 0
-
-
-
 
     def print_essential_activities(self):
         print("\nEssential activities: ")
@@ -56,19 +55,16 @@ class Graph():
         str_builder += "}"
         print(str_builder)
 
-
     @property
     def essential_activities(self):
         return self._essential_activities
 
-
     @essential_activities.setter
-    def essential_activities(self, essential_activities = None):
+    def essential_activities(self, essential_activities=None):
         if essential_activities is None:
             self.essential_activities = dict()
         else:
             self._essential_activities = essential_activities
-
 
     def __str__(self):
         str_to_print = "\nGraph print:\n"
@@ -86,27 +82,23 @@ class Graph():
     def project_duration(self):
         return int(self._project_duration)
 
-
-
     @project_duration.setter
     def project_duration(self, project_duration):
         self._project_duration = project_duration
 
-    def add_activity(self, activity_name, activity_before = [], next_activities_lst=[]):
+    def add_activity(self, activity_name, next_activities_lst=[]):
         # if activity_duration is None or activity_duration < 0:
         #      print("Activity duration must be equal or bigger than 0")
         # else:
         if activity_name != 'end':
-            activity_name = len(self.activities_dict)
-
-        if activity_name not in self.activities_dict:
-            self.activities_dict.update({activity_name : next_activities_lst})
-        else:
-            extended_lst = self.activities_dict.get(activity_name)
-            extended_lst.extend(next_activities_lst)
-            self.activities_dict.update({activity_name : extended_lst})
-        #     TODO check for duplicate dictionaries indide list
-        print("Added node successfully")
+            if activity_name not in self.activities_dict:
+                self.activities_dict.update({activity_name: next_activities_lst})
+            else:
+                extended_lst = self.activities_dict.get(activity_name)
+                extended_lst.extend(next_activities_lst)
+                self.activities_dict.update({activity_name: extended_lst})
+            #       TODO check for duplicate dictionaries inside list
+            print("Added node successfully")
 
     def remove_activity(self, activity_name):
         '''try to remove activity if activity not in dictionary, print:
@@ -140,9 +132,7 @@ class Graph():
         #              G.add_edges_from([(activity_node, connected_activities_nodes)],
         #                               weight=connected_activities_duration)
 
-
-
-        #define default style for graph
+        # define default style for graph
         # G.graph['graph'] = {'rankdir': 'TD'}
         # G.graph['node'] = {'shape': 'circle'}
         # G.graph['edges'] = {'arrowsize': '4.0'}
@@ -156,10 +146,8 @@ class Graph():
         # nx.draw_networkx_edge_labels(G,pos, edge_labels=edge_labels)
         # nx.draw(G, node_color = 'red', node_size=1500, edge_color = 'red')
         # pylab.show()
+
     #     TODO add labeles to graph (inside node's circle)
-
-
-
 
     def find_all_circles_helper(self, start_node, end_node):
         fringe = [(start_node, [])]
@@ -172,12 +160,11 @@ class Graph():
                 for next_state, next_state_duration in next_dicts.items():
                     if next_state in path:
                         continue
-                    fringe.append((next_state, path+[next_state]))
+                    fringe.append((next_state, path + [next_state]))
 
     def find_all_circles(self):
         circles = [[node] + path for node in self.activities_dict for path in self.find_all_circles_helper(node, node)]
         return circles
-
 
     def find_isolate_activities(self):
         non_isolated_activities = set()
@@ -196,7 +183,6 @@ class Graph():
                 isolated_nodes.append(node)
         return isolated_nodes
 
-
     def find_all_paths(self, start_vertex='start', end_vertex='end', path=[]):
         """ find all paths from start_vertex to
             end_vertex in graph """
@@ -210,7 +196,7 @@ class Graph():
         for next_nodes_dict in graph[start_vertex]:
             for vertex in next_nodes_dict.keys():
                 if vertex not in path:
-                    extended_paths = self.find_all_paths(vertex, end_vertex,path)
+                    extended_paths = self.find_all_paths(vertex, end_vertex, path)
                     for p in extended_paths:
                         paths.append(p)
         return paths
@@ -240,10 +226,6 @@ class Graph():
     #                                 needed_list[3] = self._es_ls_ef_lf[essential_node][3]
     #                                 self._es_ls_ef_lf[essential_node] = needed_list
 
-
-
-
-
     # def check_essentials(self, node, duration = 0):
     #     if node not in self.essential_activities:
     #         return
@@ -262,30 +244,28 @@ class Graph():
     #                     next_node_to_find = path_list[single_path_index + 1]
     #                     self.check_essentials(node)
 
-
-
-    def find_critical_path (self, start_node='start', end_node = 'end', mission = "critical path"):
+    def find_critical_path(self, start_node='start', end_node='end', mission="critical path"):
         paths = self.find_all_paths(start_node, end_node)
-        durations_lst = [0]*len(paths)
+        durations_lst = [0] * len(paths)
 
         for paths_lst_index, path_list in enumerate(paths):
             for single_path_index, node in enumerate(path_list):
-                 if node != end_node:
-                     # double check for overflow when node=end_node so when path_list[single_path_index +1] won't make problem
+                if node != end_node:
+                    # double check for overflow when node=end_node so when path_list[single_path_index +1] won't make problem
                     next_nodes_list = self.activities_dict.get(node)
                     for next_node_dict in next_nodes_list:
                         # because to get time between two  activities we need node and next node inside path
                         next_node_to_find = path_list[single_path_index + 1]
                         if node in self.essential_activities.keys():
                             self.essential_activities[node]
-                            #TODO add time by slack time
+                            # TODO add time by slack time
                             # durations_lst[paths_lst_index] +=
                         for next_node, next_node_duration in next_node_dict.items():
                             if next_node == next_node_to_find:
-                               #we search for specific node (next_node_to_find) so we can find duration between node and next_node_to_find
+                                # we search for specific node (next_node_to_find) so we can find duration between node and next_node_to_find
                                 durations_lst[paths_lst_index] += next_node_duration
                                 break
-                                #if we found next node we can break from loop
+                                # if we found next node we can break from loop
         print("\nDurations by paths:")
         print(durations_lst)
         max_duration = max(durations_lst)
@@ -293,14 +273,6 @@ class Graph():
         print("\nLongest path edges:")
         self.project_duration = max_duration
         return paths[durations_lst.index(max_duration)]
-
-
-
-
-
-
-
-
 
     # def find_critical_path(self, tmp_dict, node = 'start', lst_durations = [[]], lst_durations_index=0):
     #     if tmp_dict.get('start') == []:
@@ -317,7 +289,6 @@ class Graph():
     #     tmp_dict.get(node).pop(0) #remove first dict in list
     #     return self.find_critical_path(tmp_dict, next_node_str, lst_durations, lst_durations_index)
 
-
     # def find_critical_path(self):
     #
     #     start_lst = self.activities_dict.get('start')
@@ -330,19 +301,20 @@ class Graph():
     #             for connected_activity_node, connected_activities_duration in connected_nodes_dict.items():
     #                 self.activities_dict.get(connected_activity_node)
 
+    #########Main#########
 
-                                            #########Main#########
-g = Graph({'start': [{'2': 5}, {'3': 7 }, {'4': 6 }], '2': [{'5': 3 }, {'6': 9 }],
-           '3': [{'5': 1}, {'7': 4 }], '4': [{'7': 6}, {'6': 13 }],
-           '5': [{'end': 8}], '6': [{'end': 5}], '7': [{'end': 11 }], 'end': []})
-#g.add_activity('E', 'A', 5 )
+
+g = Graph({'start': [{'2': 5}, {'3': 7}, {'4': 6}], '2': [{'5': 3}, {'6': 9}],
+           '3': [{'5': 1}, {'7': 4}], '4': [{'7': 6}, {'6': 13}],
+           '5': [{'end': 8}], '6': [{'end': 5}], '7': [{'end': 11}], 'end': []})
+# g.add_activity('E', 'A', 5 )
 print(g)
 
 # g.remove_activity('C')
-g.add_activity('5', [{'2': 5}, {'3': 8 }])
-print(g)
+g.add_activity('5', [{'2': 5}, {'3': 8}])
+print("after adding nodes to 5", g)
 
-g.add_activity('3', [ {'B': 90} ] )
+g.add_activity('3', [{'B': 90}])
 print(g)
 # g.validate_project()
 
@@ -357,37 +329,36 @@ for isolated_node in isolated_nodes:
 g.remove_activity('C')
 print(g)
 
-
-g = Graph({'start': [{'2': 5}, {'3': 6 }, {'4': 6 }], '2': [{'5': 3 }],
-           '3': [{'5': 1}, {'6': 4}, {'7': 4 }], '4': [{'7': 13 }],
-           '5': [{'end': 8}], '6': [{'end': 5}], '7': [{'end': 11 }], 'end': []})
-print (g)
+g = Graph({'start': [{'2': 5}, {'3': 6}, {'4': 6}], '2': [{'5': 3}],
+           '3': [{'5': 1}, {'6': 4}, {'7': 4}], '4': [{'7': 13}],
+           '5': [{'end': 8}], '6': [{'end': 5}], '7': [{'end': 11}], 'end': []})
+print(g)
 isolated_nodes = g.find_isolate_activities()
 print("\nIsolated nodes:")
 for isolated_node in isolated_nodes:
     print(isolated_node)
 
-g = Graph({'start': [{'B': 5}, {'C': 7 }, {'D': 6 }], 'B': [{'E': 3 }, {'F': 9 }],
-           'C': [{'E': 1}, {'G': 4 }], 'D': [{'G': 6}, {'F': 13 }],
-           'E': [{'end': 8}], 'F': [{'end': 5}], 'G': [{'end': 11 }], 'end': []})
+g = Graph({'start': [{'B': 5}, {'C': 7}, {'D': 6}], 'B': [{'E': 3}, {'F': 9}],
+           'C': [{'E': 1}, {'G': 4}], 'D': [{'G': 6}, {'F': 13}],
+           'E': [{'end': 8}], 'F': [{'end': 5}], 'G': [{'end': 11}], 'end': []})
 
-g = Graph({'start': [{'2': 5}, {'3': 6 }, {'4': 6 }], '2': [{'5': 3 }],
-           '3': [{'5': 1}, {'6': 4}, {'7': 4 }], '4': [{'7': 13 }],
-           '5': [{'end': 8}], '6': [{'end': 5}], '7': [{'end': 11 }], 'end': []})
+g = Graph({'start': [{'2': 5}, {'3': 6}, {'4': 6}], '2': [{'5': 3}],
+           '3': [{'5': 1}, {'6': 4}, {'7': 4}], '4': [{'7': 13}],
+           '5': [{'end': 8}], '6': [{'end': 5}], '7': [{'end': 11}], 'end': []})
 
-g = Graph({'start': [{'B': 5}, {'C': 7 }, {'D': 6 }], 'B': [{'E': 3 }, {'F': 9 }],
-           'C': [{'E': 1}, {'G': 4 }], 'D': [{'G': 6}, {'F': 13 }],
-           'E': [{'end': 8}], 'F': [{'end': 5}], 'G': [{'end': 11 }], 'end': []})
+g = Graph({'start': [{'B': 5}, {'C': 7}, {'D': 6}], 'B': [{'E': 3}, {'F': 9}],
+           'C': [{'E': 1}, {'G': 4}], 'D': [{'G': 6}, {'F': 13}],
+           'E': [{'end': 8}], 'F': [{'end': 5}], 'G': [{'end': 11}], 'end': []})
 print(g)
 
 print("\nAll paths:")
 print(g.find_all_paths('start', 'end'))
 
-g = Graph({'start': [{'2': 5}, {'3': 6 }, {'4': 6 }], '2': [{'5': 3 }],
-           '3': [{'5': 1}, {'6': 4}, {'7': 4 }], '4': [{'7': 13 }],
-           '5': [{'end': 8}], '6': [{'end': 5}], '7': [{'4': 0}, {'end': 11 }], 'end': []})
+g = Graph({'start': [{'2': 5}, {'3': 6}, {'4': 6}], '2': [{'5': 3}],
+           '3': [{'5': 1}, {'6': 4}, {'7': 4}], '4': [{'7': 13}],
+           '5': [{'end': 8}], '6': [{'end': 5}], '7': [{'4': 0}, {'end': 11}], 'end': []})
 print(g)
-g.essential_activities = {'2': ['3'], 'end' : ['2'], '3': ['4']}
+g.essential_activities = {'2': ['3'], 'end': ['2'], '3': ['4']}
 g.print_essential_activities()
 print("\n All paths:")
 print(g.find_all_paths('start', 'end'))
@@ -395,7 +366,6 @@ print(g.find_critical_path('start', 'end'))
 
 print("Project duration is")
 print(g.project_duration)
-
 
 # g.fill_essential_nodes_es_ls()
 # g.print_essential_activities()
